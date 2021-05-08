2e69d509c1e33311759e69d1e7614d36855b24c2
Aditi Soni <soniaditi4623@gmail.com>
2021-05-08 22:04:58 +0530

    added stylesheets

--- src/Assets/Routes/Routes.js
+++ src/Assets/Routes/Routes.js
@@ -1,7 +1,7 @@
 import HomePage from "../../Pages/HomePage/HomePage";
 import SignUp from "../../Pages/SignUp/SignUp";
 import LogIn from "../../Pages/LogIn/Login";
-import Game1 from "../../Pages/Game1/Game1";
+import Game1 from "../../Pages/Game1/Game";
 import Game3 from "../../Pages/Game3/Game3";
 import Game2 from "../../Pages/Game2/Game2";
 export const Routes = [

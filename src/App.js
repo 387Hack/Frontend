436b9dae2259f387eba4a4607de5e00d278c647a
mkp0 <shadymasum@gmail.com>
2021-05-09 02:56:36 +0530

    styling game2 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Routes } from "./Assets/Routes/Routes";
 import Navbar from "./Components/Navbar/Navbar";
 import "bulma/css/bulma.min.css";
+import "semantic-ui-css/semantic.min.css";
 import { GlobalStyles } from "./Assets/styles/GlobalStyles";
 function App() {
   return (
--- src/Pages/Game2/Game2.js
+++ src/Pages/Game2/Game2.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
 import bell from "../../Assets/Audio/bell.wav";
 import { randomNo } from "./Game2.style";
+import { Button, Input, Card, Icon, Image } from "semantic-ui-react";
 export default function Game2() {
   const [bellSound] = useState(new Audio(bell));
   const [level, setLevel] = useState(0);
   const [Count, setCount] = useState(0);
   const [userInput, setUserInput] = useState("");
-  const [status, setStatus] = useState("");
+  const [status, setStatus] = useState("Let's Play");
   const [loading, setLoading] = useState(false);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
-    bellSound.playbackRate = 1.3 + level * 0.15;
+    if (level === 0) {
+      return;
+    }
+    bellSound.playbackRate = 1.3 + level * 0.25;
 
     bellSound.play();
     let count = 0;
-    let loops = Count - 1;
+    let loops = 1 - 1;
     console.log(loops);
     bellSound.onended = function () {
       if (count < loops) {
@@ -33,6 +38,10 @@ export default function Game2() {
     setLoading(true);
     setCount(randomNo(level + 3, level + 6));
     setLevel(level + 1);
+    if (level === 10) {
+      setStatus(`Your score is ${score}`);
+      setLevel(0);
+    }
   };
 
   const onSubmit = () => {
@@ -41,6 +50,7 @@ export default function Game2() {
     console.log(counts, Count);
     if (counts === Count) {
       setStatus("Correct");
+      setScore(score + 1);
     } else {
       setStatus("Incorrect");
     }
@@ -49,19 +59,45 @@ export default function Game2() {
   return (
     <div>
       <h1>Hearing Game</h1>
-      <div>
-        <div>
-          <button onClick={nextHandler}>{level ? "Next" : "Start"}</button>
-        </div>
-        <input
-          onChange={(e) => {
-            setUserInput(e.target.value);
-          }}
-          value={userInput}
-        ></input>
-        <button onClick={onSubmit}>Submit</button>
+
+      <div
+        style={{ margin: "auto", display: "flex", justifyContent: "center" }}
+      >
+        <Card>
+          <div style={{ fontSize: "1.8rem" }}>{status}</div>
+          <Image src="https://picsum.photos/200/150" wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Hey</Card.Header>
+            <Card.Description>
+              <b>Guess the Count of Sound</b>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name="chart pie" />
+            Level : {level}
+          </Card.Content>
+          <div>
+            <Button style={{ fontSize: "1.3rem" }} onClick={nextHandler}>
+              {level ? "Next" : "Start"}
+            </Button>
+          </div>
+          <div style={{ padding: "0 10%", margin: "1rem 0" }}>
+            <Input
+              onChange={(e) => {
+                setUserInput(e.target.value);
+              }}
+              value={userInput}
+              style={{ margin: 0, padding: 0 }}
+              placeholder="Predict the Count"
+            ></Input>
+          </div>
+          <div>
+            <Button style={{ fontSize: "1.2rem" }} onClick={onSubmit}>
+              Submit
+            </Button>
+          </div>
+        </Card>
       </div>
-      {status}
     </div>
   );
 }
